e937e806227367b71cb2d3fe8ec7f6c756d2c08c
Rakib12345678 <88559550+Rakib12345678@users.noreply.github.com>
2024-02-01 00:26:31 +0530

    Updated base url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = "https://dress-shopping-server-production.up.railway.app/"
+export const API_BASE_URL = "https://dress-shopping-server-production.up.railway.app"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
