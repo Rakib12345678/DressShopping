f9e91eb3f40913684400aa952ef77b9b57af5e3f
Rakib12345678 <88559550+Rakib12345678@users.noreply.github.com>
2024-02-03 17:32:06 +0530

    Update HomeProductCard.jsx

--- src/customer/Components/Home/HomeProductCard.jsx
+++ src/customer/Components/Home/HomeProductCard.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
 import { useNavigate } from "react-router-dom";
 
-const HomeProductCard = ({ product }) => {
+const HomeProductCard = ({ product,section }) => {
   const navigate = useNavigate();
+  const [endpoint,setEndpoint] = useState("");
+  useEffect(()=>{
+    switch(section){
+      case 0:{
+        setEndpoint("men/clothing/mens_kurta");
+        break;
+      }
+      case 1:{
+      }
+      case 2:{
+        setEndpoint("women/clothing/saree");
+        break;
+      }
+      case 3:{
+        setEndpoint("women/clothing/women_dress")
+        break;
+      }
+      case 4:
+      case 5:{
+        setEndpoint("women/clothing/kurtas")
+        break;
+      }
+    }
+  },[section])
 
   return (
     <div
-      onClick={() => navigate(`/men/clothing/mens_kurta`)}
+      onClick={() => navigate(endpoint)}
       className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3"
     >
       <div className="h-[13rem] w-[10rem]">
